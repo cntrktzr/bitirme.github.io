6c42276a70e91fc292361e98e055069582e39ebb
cntrktzr <70589341+cntrktzr@users.noreply.github.com>
2022-06-02 19:50:45 +0300

    devam

--- app.js
+++ app.js
@@ -1,163 +1,180 @@
-const dotenv = require('dotenv').config();
-const express = require ('express');
+const dotenv = require("dotenv").config();
+const express = require("express");
 const app = express();
-const session = require('express-session');
-const flash = require('connect-flash');
-const passport = require('passport');
-const http=require('http'); 
-const socketio=require('socket.io');
-const formatMessage=require('./public/admin/js/messages');
-const { format } = require('path');
-const  {userJoin,getCurrentUser, getRoomUsers,userLeave }=require('./public/admin/js/users');
-const server=http.createServer(app);
-const io=socketio(server);
+const session = require("express-session");
+const flash = require("connect-flash");
+const passport = require("passport");
+const http = require("http");
+const socketio = require("socket.io");
+const formatMessage = require("./public/admin/js/messages");
+const { format } = require("path");
+const {
+  userJoin,
+  getCurrentUser,
+  getRoomUsers,
+  userLeave,
+} = require("./public/admin/js/users");
+const server = http.createServer(app);
+const io = socketio(server);
 const PORT = 3000 || process.env.PORT;
-const {TranslationServiceClient} = require('@google-cloud/translate');
-const {Translate} = require('@google-cloud/translate').v2;
+const { TranslationServiceClient } = require("@google-cloud/translate");
+const { Translate } = require("@google-cloud/translate").v2;
 
-const admin = 'T&I';
-io.on('connection', (socket, language)=>{
+const admin = "T&I";
+io.on("connection", (socket, language) => {
+  socket.on("joinRoom", ({ username, room, language }) => {
+    //to join a room
+    const user = userJoin(socket.id, username, room, language);
+    console.info(`Client's language is [language=${language}]`);
 
-    socket.on('joinRoom',({username,room, language})=>{
+    socket.join(user.room);
 
-        //to join a room
-        const user=userJoin(socket.id,username,room, language);
-        console.info(`Client's language is [language=${language}]`)
-        
-        socket.join(user.room);
-        
-        socket.emit('message' , formatMessage(admin,`Welcome to the chat :). Your selected language is "${user.language}".`));
-        
-        socket.broadcast.to(user.room).emit('message', formatMessage(admin,`${user.username} has joined the chat!`));
+    socket.emit(
+      "message",
+      formatMessage(
+        admin,
+        `Welcome to the chat :). Your selected language is "${user.language}".`
+      )
+    );
 
-        // Send users and room info
-        io.to(user.room).emit('roomUsers',{
-            language:`${user.language}`,
-            room:user.room,
-            users:getRoomUsers(user.room),
-        });
-        
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        "message",
+        formatMessage(admin, `${user.username} has joined the chat!`)
+      );
+
+    // Send users and room info
+    io.to(user.room).emit("roomUsers", {
+      language: `${user.language}`,
+      room: user.room,
+      users: getRoomUsers(user.room),
     });
-    
-    // Listen for the chat message 
-    socket.on('chatMessage',(msg)=>{
-    const user=getCurrentUser(socket.id, language);
+  });
 
-    if(`${user.language}` == 'en'){
-        socket.emit('message' , formatMessage(user.username, msg));
+  // Listen for the chat message
+  socket.on("chatMessage", (msg) => {
+    const user = getCurrentUser(socket.id, language);
 
-    }
-    else {
-        const translationClient = new TranslationServiceClient();
-        const projectId = 'bitirme-projesi-348016';
-        const location = 'global';
-        const text = msg;
+    if (`${user.language}` == "en") {
+      socket.emit("message", formatMessage(user.username, msg));
+    } else {
+      const translationClient = new TranslationServiceClient();
+      const projectId = "bitirme-projesi-348016";
+      const location = "global";
+      const text = msg;
 
-        async function translateText() {
-            const request = {
-                parent: `projects/${projectId}/locations/${location}`,
-                contents: [text],
-                mimeType: 'text/html',
-                sourceLanguageCode: `${user.language}`,
-                targetLanguageCode: 'en',
-            };
-            const [response] = await translationClient.translateText(request);
-        
-            for (const translation of response.translations) {
-                socket.emit('message',formatMessage(user.username, msg)); // only sender
-                socket.broadcast.to(user.room).emit('message',formatMessage(user.username,`${translation.translatedText}`)); //except sender
-                console.log(`Translation: ${translation.translatedText}`);
-            }
+      async function translateText() {
+        const request = {
+          parent: `projects/${projectId}/locations/${location}`,
+          contents: [text],
+          mimeType: "text/html",
+          sourceLanguageCode: `${user.language}`,
+          targetLanguageCode: "en",
+        };
+        const [response] = await translationClient.translateText(request);
+
+        for (const translation of response.translations) {
+          socket.emit("message", formatMessage(user.username, msg)); // only sender
+          socket.broadcast
+            .to(user.room)
+            .emit(
+              "message",
+              formatMessage(user.username, `${translation.translatedText}`)
+            ); //except sender
+          console.log(`Translation: ${translation.translatedText}`);
         }
-        translateText();  
+      }
+      translateText();
     }
-    
-    });
+  });
 
-     // User disconnects
-    socket.on('disconnect',()=>{
+  // User disconnects
+  socket.on("disconnect", () => {
+    const user = userLeave(socket.id);
+    if (user) {
+      io.to(user.room).emit(
+        "message",
+        formatMessage(admin, `${user.username} has left the chat`)
+      );
 
-        const user=userLeave(socket.id);
-        if(user){
-            io.to(user.room).emit('message', formatMessage(admin,`${user.username} has left the chat`));
-
-
-            io.to(user.room).emit('roomUsers',{
-                room:user.room,
-                users:getRoomUsers(user.room)
-            });
-        }     
-    });
-
+      io.to(user.room).emit("roomUsers", {
+        room: user.room,
+        users: getRoomUsers(user.room),
+      });
+    }
+  });
 });
 
-const ejs = require('ejs');
-const expressLayout = require('express-ejs-layouts');
-const path = require('path');
+const ejs = require("ejs");
+const expressLayout = require("express-ejs-layouts");
+const path = require("path");
 app.use(expressLayout);
-app.use(express.static('public'));
-app.set('view engine', 'ejs');
-app.set('views', path.resolve(__dirname, './src/views'));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "./src/views"));
 
 //Database bağlantısı
-require('./src/config/database')
-const MongoDBStore = require('connect-mongodb-session')(session);
+require("./src/config/database");
+const MongoDBStore = require("connect-mongodb-session")(session);
 
 const sessionStore = new MongoDBStore({
   uri: process.env.MONGODB_CONNECTION_STRING,
-    collection: 'Sessions'
+  collection: "Sessions",
 });
 
-app.use(session({
+app.use(
+  session({
     secret: process.env.SESSION_SECRET,
-    resave : false,
+    resave: false,
     saveUninitialized: true,
-    cookie : {
-        maxAge : 1000 * 60 * 60 * 24
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24,
     },
-    store : sessionStore
-}));
+    store: sessionStore,
+  })
+);
 
 app.use(flash());
 
-app.use((req, res, next) =>{
-    res.locals.validation_error = req.flash('validation_error');
-    res.locals.success_message = req.flash('success_message')
-    res.locals.email = req.flash('email');
-    res.locals.name = req.flash('name');
-    res.locals.lastname = req.flash('lastname');
-    res.locals.password = req.flash('password');
-    res.locals.rpassword = req.flash('rpassword');
-    res.locals.login_error = req.flash('error');
-    next();
-})
+app.use((req, res, next) => {
+  res.locals.validation_error = req.flash("validation_error");
+  res.locals.success_message = req.flash("success_message");
+  res.locals.email = req.flash("email");
+  res.locals.name = req.flash("name");
+  res.locals.lastname = req.flash("lastname");
+  res.locals.password = req.flash("password");
+  res.locals.rpassword = req.flash("rpassword");
+  res.locals.login_error = req.flash("error");
+  next();
+});
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-const authRouter = require('./src/routers/auth_router');
-const userRouter = require('./src/routers/user_router');
-const girisRouter = require('./src/routers/giris_router');
-const joinRouter = require('./src/routers/join_router');
-const chatRouter = require('./src/routers/chat_router');
+const authRouter = require("./src/routers/auth_router");
+const userRouter = require("./src/routers/user_router");
+const girisRouter = require("./src/routers/giris_router");
+const joinRouter = require("./src/routers/join_router");
+const chatRouter = require("./src/routers/chat_router");
 
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }));
 
-const res = require('express/lib/response');
-const { $where } = require('./src/model/user_model');
-const { resolveAny } = require('dns');
+const res = require("express/lib/response");
+const { $where } = require("./src/model/user_model");
+const { resolveAny } = require("dns");
 
-app.get('/', (req, res) =>{
-    res.json({mesaj : 'Merhaba'});
-})
+app.get("/", (req, res) => {
+  res.json({ mesaj: "Merhaba" });
+});
 
-app.use('/', authRouter);
-app.use('/giris' , girisRouter);
-app.use('/join', joinRouter);
-app.use('/chat', chatRouter);
+app.use("/", authRouter);
+app.use("/giris", girisRouter);
+app.use("/join", joinRouter);
+app.use("/chat", chatRouter);
 
-app.use('/user', userRouter);
+app.use("/user", userRouter);
 
-server.listen(PORT, () =>{
-    console.log(`Server running on ${PORT}..`);
+server.listen(PORT, () => {
+  console.log(`Server running on ${PORT}..`);
 });
--- public/admin/js/main.js
+++ public/admin/js/main.js
@@ -1,102 +1,98 @@
-// This is the front end js file 
+// This is the front end js file
 
-const chatForm=document.getElementById('chat-form');
-const chatMessages=document.querySelector('.chat-messages');
-const selectedLanguage=document.getElementById('selected-language');
-const roomName=document.getElementById('room-name');
-const userList=document.getElementById('users');
+const chatForm = document.getElementById("chat-form");
+const chatMessages = document.querySelector(".chat-messages");
+const selectedLanguage = document.getElementById("selected-language");
+const roomName = document.getElementById("room-name");
+const userList = document.getElementById("users");
 
-const { username, room, language}= Qs.parse(location.search,{
-    ignoreQueryPrefix:true
+const { username, room, language } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true,
 });
 
-//We now have access to the front end socket here 
-const socket=io();
+//We now have access to the front end socket here
+const socket = io();
 
-//send the room name and user name to the server 
-socket.emit('joinRoom',{username,room, language});
+//send the room name and user name to the server
+socket.emit("joinRoom", { username, room, language });
 
 //Get room and users
-socket.on('roomUsers',({users,room, language})=>{
-    outputLanguage(language);
-    outputRoomName(room);
-    outputUsers(users);  
-    
+socket.on("roomUsers", ({ users, room, language }) => {
+  outputLanguage(language);
+  outputRoomName(room);
+  outputUsers(users);
 });
 
 // Catch the message here
-socket.on('message', message=>{
-    console.log(message);
-    outputMessage(message);
-    chatMessages.scrollTop=chatMessages.scrollHeight;
+socket.on("message", (message) => {
+  console.log(message);
+  outputMessage(message);
+  chatMessages.scrollTop = chatMessages.scrollHeight;
 });
 
 //Submit the form (Handwriting)
-chatForm.addEventListener('submit',(e)=>{
-    e.preventDefault();
-    //gettting the message
-    const msg = e.target.elements.msg.value;
-    //Emit message to the server
-    socket.emit('chatMessage', msg);
-    e.target.elements.msg.value='';
-    e.target.elements.msg.focus();
+chatForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  //gettting the message
+  const msg = e.target.elements.msg.value;
+  //Emit message to the server
+  socket.emit("chatMessage", msg);
+  e.target.elements.msg.value = "";
+  e.target.elements.msg.focus();
 });
 
 // Submit the form (Voice Recognition)
-chatForm.addEventListener('submit',(e)=>{
-    e.preventDefault();
-    //gettting the message
-    const output = e.target.elements.output.value;
-    //Emit message to the server
-    socket.emit('chatMessage', output);
-    e.target.elements.output.value='';
-    e.target.elements.output.focus();
+chatForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  //gettting the message
+  const output = e.target.elements.output.value;
+  //Emit message to the server
+  socket.emit("chatMessage", output);
+  e.target.elements.output.value = "";
+  e.target.elements.output.focus();
 });
 
-// Output message to    
-function outputMessage(message){
-    const div = document.createElement('div');
-    
-    div.classList.add('message');
-    console.log(`Ahmet ${message.textMessage}`)
-    div.innerHTML=` <body>
+// Output message to
+function outputMessage(message) {
+  const div = document.createElement("div");
+
+  div.classList.add("message");
+  console.log(`Ahmet ${message.textMessage}`);
+  div.innerHTML = ` <body>
     <p class="meta">${message.username}<span>${message.time}</span></p>
     <p id="mesaj" class="text">
         ${message.textMessage}
         <button id="text-audio" class="imaj" type="button" onclick="textToAudio()"><img src="admin/img/hoparlor.jpeg" ></button>   
     </p>
     </body>`;
-    
-    document.querySelector('.chat-messages').appendChild(div);
-}    
+
+  document.querySelector(".chat-messages").appendChild(div);
+}
 
 function textToAudio() {
-    const text = document.getElementById('mesaj').value;
-    let speech = new SpeechSynthesisUtterance();
-    speech.lang = "en-US";
-        
-    speech.text = text;
-    speech.volume = 1;
-    speech.rate = 1;
-    speech.pitch = 1;
-    
-    window.speechSynthesis.speak(speech);
+  const text = document.getElementById("mesaj").value;
+  let speech = new SpeechSynthesisUtterance();
+  speech.lang = "en-US";
+
+  speech.text = text;
+  speech.volume = 1;
+  speech.rate = 1;
+  speech.pitch = 1;
+
+  window.speechSynthesis.speak(speech);
 }
 
-
-
-function outputLanguage(language){
-    selectedLanguage.innerText=language;
+function outputLanguage(language) {
+  selectedLanguage.innerText = language;
 }
-
 
 //Output room name to DOM
-function outputRoomName(room){
-    roomName.innerText=room;
+function outputRoomName(room) {
+  roomName.innerText = room;
 }
 
-function outputUsers(users){
-    userList.innerHTML=`
-    ${users.map(user=>`<li>${user.username}</li>`).join('')}
+function outputUsers(users) {
+  userList.innerHTML = `
+    ${users.map((user) => `<li>${user.username}</li>`).join("")}
     `;
 }
